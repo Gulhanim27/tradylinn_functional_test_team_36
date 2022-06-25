2ad7f4b585c43d910fcb3cbabd4fd8cf4ac88a69
GUL27 <anulurgulhanim@gmail.com>
2022-06-24 22:39:30 -0400

    deneme

--- src/test/java/pages/Gulhanim.java
+++ src/test/java/pages/Gulhanim.java
@@ -12,4 +12,5 @@ public class Gulhanim {
 
     @FindBy(xpath="locater")
     public WebElement locaterName;
+
 }
--- src/test/java/tests/tradylinn/T01_Aydin.java
+++ src/test/java/tests/tradylinn/T01_Aydin.java
@@ -1,8 +1,9 @@
 package tests.tradylinn;
 
 import org.testng.annotations.Test;
+import utilities.TestBase;
 
-public class T01_Aydin {
+public class T01_Aydin{
     @Test
     public void test01(){
 

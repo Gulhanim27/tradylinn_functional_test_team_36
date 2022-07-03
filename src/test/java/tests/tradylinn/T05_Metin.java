08c51acb33b046cd7cb8db854479209ccd9e6558
Zeynep-bak <zeynepsimsek81@mail.com>
2022-07-03 11:11:03 +0200

    ucuncu commit

--- src/test/java/tests/tradylinn/T05_Metin.java
+++ src/test/java/tests/tradylinn/T05_Metin.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.Metin;
@@ -11,6 +14,7 @@ import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.TestBase;
 
+import java.time.Duration;
 import java.util.List;
 
 public class T05_Metin extends TestBase {
@@ -19,11 +23,13 @@ public class T05_Metin extends TestBase {
 
       Actions actions=new Actions(Driver.getDriver());
         Metin metinSayfasi = new Metin();
+        WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(15));
+
 
             //1-Kullanici tradylinn url sayfasina gider
             Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
             //2- kullanici hesabim butonunu tiklar
-            Thread.sleep(3000);
+        wait.until(ExpectedConditions.elementToBeClickable(metinSayfasi.hesabimButonu));
             metinSayfasi.hesabimButonu.click();
             Thread.sleep(2000);
             //3-Kullanici gecerli email adresini girer
@@ -31,27 +37,34 @@ public class T05_Metin extends TestBase {
             //4-Kullanici gecerli password girer
             metinSayfasi.password.sendKeys(ConfigReader.getProperty("password"));
             //5-Kullanici giris yap butonunu tiklar
-            actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
-            actions.click(metinSayfasi.girisYapButonu);
-            Thread.sleep(3000);
+            actions.sendKeys(Keys.PAGE_DOWN).perform();
+            metinSayfasi.girisYapButonu.click();
+            Thread.sleep(2000);
+        wait.until(ExpectedConditions.elementToBeClickable(metinSayfasi.storeManager));
         //Kullanici store manager butonunu tiklar
-            actions.click(metinSayfasi.storeManager);
+            metinSayfasi.storeManager.click();
         //Kullanici urun butonunu tiklar
-            actions.click(metinSayfasi.urunButonu);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+            metinSayfasi.urunButonu.click();
+            wait.until(ExpectedConditions.elementToBeClickable(metinSayfasi.yeniEkleButonu));
         //Kullanici yeni ekle butonunu tiklar
-            actions.click(metinSayfasi.yeniEkleButonu);
+            metinSayfasi.yeniEkleButonu.click();
 
         //Kullanici sayfanin altindaki Attributes butonunu tiklar
-        actions.sendKeys(Keys.PAGE_DOWN);
-        actions.click(metinSayfasi.attributesButonu);
+
+        actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
+        wait.until(ExpectedConditions.elementToBeClickable(metinSayfasi.attributesButonu));
+        metinSayfasi.attributesButonu.click();
         //Kullanici color check box ini tiklar
-        actions.click(metinSayfasi.colorCheckBox);
+        wait.until(ExpectedConditions.elementToBeClickable(metinSayfasi.colorCheckBox));
+        metinSayfasi.colorCheckBox.click();
         //Kullanici SELECT ALL butonunu tiklar
-        actions.click( metinSayfasi.colorSelectAll);
+        wait.until(ExpectedConditions.elementToBeClickable(metinSayfasi.colorSelectAll));
+        metinSayfasi.colorSelectAll.click();
         //Kullanici renkleri assert eder
-   List<WebElement> liste = Driver.getDriver().findElements(By.xpath("//*[@class='select2-selection__choice']"));
+   List<WebElement> liste = Driver.getDriver().findElements(By.xpath("//li[@class='select2-selection__choice']"));
     for (WebElement list : liste) {
-        Assert.assertTrue(list.isDisplayed());
+        System.out.print(list.getText());
     }
 
 
